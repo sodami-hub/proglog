90d73e7e6eaba9202ed97885c6277691faeadd22
ganzigood <good4ram@naver.com>
2025-01-26 23:14:42 +0900

    gRPC 서버  테스트코드 작성+

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -1,1 +1,35 @@
 package server
+
+import (
+	"net"
+	"testing"
+
+	api "github.com/sodami-hub/proglog/api/v1"
+	"github.com/stretchr/testify/require"
+)
+
+func TestServ(t *testing.T) {
+	for scenario, fn := range map[string]func(
+		t *testing.T,
+		client api.LogClient,
+		config *Config,
+	){
+		"produce/consume a message to/from the log succeeds": testProduceConsume,
+		"produce/consume stream succeeds":                    testProduceConsumeStream,
+		"consume past log boundary fails":                    testConsumePastBoundary,
+	} {
+		t.Run(scenario, func(t *testing.T) {
+			client, config, teardown := setupTest(t, nil)
+			defer teardown()
+			fn(t, client, config)
+		})
+	}
+}
+
+func setupTest(t *testing.T, fn func(*Config)) (client api.LogClient, cfg *Config, teardown func()) {
+	t.Helper()
+
+	l, err := net.Listen("tcp", ":0")
+	require.NoError(t, err)
+
+}
